bdaaba2095266f30396692fee97be0bdedb3dd90
Michael Lechner <xsltwonder@gmail.com>
2026-03-19 17:23:26 +0000

    ci: fix Linux tests by excluding GUI via build tags and removing obsolete folder

--- cmd/gui/main.go
+++ cmd/gui/main.go
@@ -1,6 +1,8 @@
 // Copyright (c) 2026 Michael Lechner
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+//go:build windows || darwin
+
 package main
 
 import (
